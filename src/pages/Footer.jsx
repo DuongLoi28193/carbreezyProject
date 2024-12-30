73f2fdc07ebe9a3ea9389d83da64a3b25ca5b53a
MTH <chunhau.py@gmail.com>
2024-12-30 20:33:43 +0700

    update

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { faFacebook, faYoutube, faXTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
