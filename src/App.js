e47ca5310771d512450ca8ea501f645343f496ca
MTH <chunhau.py@gmail.com>
2025-01-02 15:18:18 +0700

    res tablet Home

--- src/App.js
+++ src/App.js
@@ -107,7 +107,7 @@ function App() {
         {/* navbar for tablet & mobile */}
         <div className='mobile-tablet_menu'>
           <div className='res_header-menu'>
-            <img className='logo logo-menu' src="../img/Group_193-removebg-preview.png" alt="" />
+            <img className='logo logo-menu' src={process.env.PUBLIC_URL + "/img/Group_193-removebg-preview.png"} alt="" />
             <label htmlFor="check-menu">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7">
                 <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
